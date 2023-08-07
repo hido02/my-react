a5354aed88378d1dd248c6f37f11955fd93ec7e3
hido02 <91791632+hido02@users.noreply.github.com>
2023-08-07 12:47:31 +0900

    feat: useReducer 카운터 예시

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 function App() {
-  function FriendStatus(props) {
-    const [isOnline, setIsOnline] = useState(null);
-  
-    function handleStatusChange(status) {
-      setIsOnline(status.isOnline);
+  const initialState = {count: 0};
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'increment':
+        return {count: state.count + 1};
+      case 'decrement':
+        return {count: state.count - 1};
+      default:
+        throw new Error();
     }
+  }
   
-    useEffect(() => {
-      ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange);
-      return () => {
-        ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange);
-      };
-    });
-  
-    if (isOnline === null) {
-      return 'Loading...';
-    }
-    return isOnline ? 'Online' : 'Offline';
+  function Counter() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    return (
+      <>
+        Count: {state.count}
+        <button onClick={() => dispatch({type: 'decrement'})}>-</button>
+        <button onClick={() => dispatch({type: 'increment'})}>+</button>
+      </>
+    );
   }
   
   return (
     <>
-    <FriendStatus />
+    <Counter />
     </>
   );
 }
